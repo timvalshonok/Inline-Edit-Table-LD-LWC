8d8fc98d1020f388906fa7044a8ae767337dbf3b
tsimaphei <tima.valshonok@gmail.com>
2021-09-12 16:24:56 +0300

    4 project ld

---
+++ force-app/main/default/lwc/customDataTable/customDataTable.js
@@ -0,0 +1,13 @@
+import LightningDatatable from 'lightning/datatable';
+import DatatablePicklistTemplate from './picklistTemplate.html';
+
+export default class CustomDatatable extends LightningDatatable {
+    
+    static customTypes = {
+        picklist: {
+            template: DatatablePicklistTemplate,
+            standardCellLayout: true,
+            typeAttributes: ['label', 'placeholder', 'options', 'value', 'context', 'variant','name']
+        }
+    };
+}
---
+++ force-app/main/default/lwc/inlineEditTable/inlineEditTable.js
@@ -0,0 +1,236 @@
+import { LightningElement, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getAccounts from '@salesforce/apex/InlineEditTableController.getAccounts';
+import saveDraftValues from '@salesforce/apex/InlineEditTableController.saveDraftValues';
+import { deleteRecord } from 'lightning/uiRecordApi';
+
+const COLUMNS = [
+    { label: 'Name', fieldName: 'Name', type: 'text', editable: true },
+    { label: 'Rating', fieldName: 'Rating', type: 'picklist', editable: true,
+        typeAttributes: { placeholder: 'Choose Rating', options: [{ label: 'Hot', value: 'Hot' }, { label: 'Warm', value: 'Warm' }, { label: 'Cold', value: 'Cold' }],
+            value: { fieldName: 'Rating' }, context: { fieldName: 'Id' }, variant: 'label-hidden', name: 'Rating', label: 'Rating' },
+        cellAttributes: { class: { fieldName: 'stageClass' }}
+    },
+    { type: 'button-icon', typeAttributes: { iconName: 'utility:delete', name: 'delete', iconClass: 'slds-icon-text-error' }}
+];
+
+export default class InlineEditTable extends LightningElement {
+    
+    columns = COLUMNS;
+    records;
+    lastSavedData;
+    error;
+    accountId;
+    wiredRecords;
+    showSpinner = false;
+    draftValues = [];
+    privateChildren = {}; //used to get the datatable picklist as private childern of customDatatable
+
+    //get accounts
+    @wire(getAccounts)
+    wiredAccountsRecords(result){
+        this.wiredRecords = result;
+        const { data, error } = result;
+        if (data) {
+            this.records = JSON.parse(JSON.stringify(data));
+            this.records.forEach(record => {
+                record.stageClass = 'slds-cell-edit';
+            })
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.records = undefined;
+        }
+        this.lastSavedData = this.records;
+        this.showSpinner = false;
+    }
+
+    handleEdit(event) {
+        event.preventDefault();
+        let dataRecieved = event.detail.data;
+        this.handleWindowOnclick(dataRecieved.context);
+        switch (dataRecieved.label) {
+            case 'Rating':
+                this.setClassesOnData(
+                    dataRecieved.context,
+                    'stageClass',
+                    'slds-cell-edit'
+                );
+                break;
+            default:
+                this.setClassesOnData(dataRecieved.context, '', '');
+                break;
+        };
+    }
+
+    setClassesOnData(id, fieldName, fieldValue) {
+        this.records = JSON.parse(JSON.stringify(this.records));
+        this.records.forEach((detail) => {
+            if (detail.Id === id) {
+                detail[fieldName] = fieldValue;
+            }
+        });
+    }
+
+    handleCellChange(event) {
+        event.preventDefault();
+        this.updateDraftValues(event.detail.draftValues[0]);
+    }
+
+    // Event to register the datatable picklist mark up
+    handleItemRegister(event) {
+        event.stopPropagation(); //stops the window click to propagate to allow to register of markup
+        const item = event.detail;
+        if (!this.privateChildren.hasOwnProperty(item.name))
+            this.privateChildren[item.name] = {};
+        this.privateChildren[item.name][item.guid] = item;
+    }
+
+    //Captures the changed picklist value and updates the records list variable
+    handleValueChange(event) {
+        event.stopPropagation();
+        let dataRecieved = event.detail.data;
+        let updatedItem;
+        switch (dataRecieved.label) {
+            case 'Rating':
+                updatedItem = {
+                    Id: dataRecieved.context,
+                    Rating: dataRecieved.value
+                };
+                //Set the cell edit class to edited to mark it as value changed
+                this.setClassesOnData(
+                    dataRecieved.context,
+                    'stageClass',
+                    'slds-cell-edit slds-is-edited'
+                );
+                break;
+            default:
+                this.setClassesOnData(dataRecieved.context, '', '');
+                break;
+        }
+        this.updateDraftValues(updatedItem);
+        this.updateDataValues(updatedItem);
+    }
+
+    updateDataValues(updateItem) {
+        let copyData = JSON.parse(JSON.stringify(this.records));
+        copyData.forEach((item) => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+            }
+        });
+        this.records = [...copyData];
+    }
+
+    updateDraftValues(updateItem) {
+        let draftValueChanged = false;
+        let copyDraftValues = JSON.parse(JSON.stringify(this.draftValues));
+        copyDraftValues.forEach((item) => {
+            if (item.Id === updateItem.Id) {
+                for (let field in updateItem) {
+                    item[field] = updateItem[field];
+                }
+                draftValueChanged = true;
+            }
+        });
+        if (draftValueChanged) {
+            this.draftValues = [...copyDraftValues];
+        } else {
+            this.draftValues = [...copyDraftValues, updateItem];
+        }
+    }
+
+    handleSave(event) {
+        event.preventDefault();
+        this.showSpinner = true;
+        // Update draftvalues
+        saveDraftValues({ data: this.draftValues })
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Accounts updated successfully',
+                        variant: 'success'
+                    })
+                );
+                //Get the updated list with refreshApex and added cell edit class
+                refreshApex(this.wiredRecords).then(() => {
+                    this.records.forEach(record => {
+                        record.accountNameClass = 'slds-cell-edit';
+                        record.stageClass = 'slds-cell-edit';
+                    });
+                    this.draftValues = [];
+                });
+            })
+            .catch(error => {
+                console.log('error : ' + JSON.stringify(error));
+                this.showSpinner = false;
+            });
+    }
+
+    handleCancel(event) {
+        event.preventDefault();
+        this.records = JSON.parse(JSON.stringify(this.lastSavedData));
+        this.handleWindowOnclick('reset');
+        this.draftValues = [];
+    }
+
+    //delete row
+    handleRowAction(event) {
+        if (event.detail.action.name === 'delete') {
+            const record = event.detail.row;
+            deleteRecord(record.Id)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Record deleted',
+                        variant: 'success'
+                    })
+                );
+                refreshApex(this.wiredRecords);
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error deleting record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            })    
+        }
+    }
+
+    renderedCallback() {
+        if (!this.isComponentLoaded) {
+            /* Add Click event listener to listen window click to reset the picklist selection 
+            to text view if context is out of sync*/
+            window.addEventListener('click', (evt) => {
+                this.handleWindowOnclick(evt);
+            });
+            this.isComponentLoaded = true;
+        }
+    }
+
+    handleWindowOnclick(context) {
+        this.resetPopups('c-datatable-picklist', context);
+    }
+
+    //create object value of datatable picklist markup to allow to call callback function with window click event listener
+    resetPopups(markup, context) {
+        let elementMarkup = this.privateChildren[markup];
+        if (elementMarkup) {
+            Object.values(elementMarkup).forEach((element) => {
+                element.callbacks.reset(context);
+            });
+        }
+    }
+
+    disconnectedCallback() {
+        window.removeEventListener('click', () => { });
+    }
+}
